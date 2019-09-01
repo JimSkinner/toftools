65e8c771b6e5fcf4e7c4979c5937ef6ae81af4f3
Jim Skinner <jskinner.mail@gmail.com>
2019-09-01 11:14:09 +0100

    Test TOF on an extra file

--- tests/testthat/test-end-to-end.R
+++ tests/testthat/test-end-to-end.R
@@ -1,8 +1,17 @@
-context("system tests")
+context("system tests: TOF")
 
-test_that("Calling 'analyse' on a TOF file produces a report without errors", {
+test_that("Calling 'analyse' on a TOF file (coeliac_height) produces a report without errors", {
   infile <- "../../data-raw/coeliac_height.txt"
   outfile <- "../../data-raw/coeliac_height.html"
+  analyse(infile)
+
+  expect_true(file.exists(outfile))
+  file.remove(outfile)
+})
+
+test_that("Calling 'analyse' on a TOF file (sepsis_area) produces a report without errors", {
+  infile <- "../../data-raw/sepsis_area.txt"
+  outfile <- "../../data-raw/sepsis_area.html"
   analyse(infile)
 
   expect_true(file.exists(outfile))
