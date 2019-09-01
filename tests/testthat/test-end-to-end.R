468127033dd0f190f7e6858307d84e096d462d6a
Jim Skinner <jskinner.mail@gmail.com>
2019-09-01 12:01:26 +0100

    System test for GCIMS

--- tests/testthat/test-end-to-end.R
+++ tests/testthat/test-end-to-end.R
@@ -3,24 +3,48 @@ context("system tests: TOF")
 test_that("Calling 'analyse' on a TOF file (coeliac_height) produces a report without errors", {
   infile <- "../../data-raw/coeliac_height.txt"
   outfile <- "../../data-raw/coeliac_height.html"
+
+  suppressWarnings(file.remove(outfile)) # Existing file can hide failed test
+
   analyse(infile)
 
   expect_true(file.exists(outfile))
-  file.remove(outfile)
+  file.remove(outfile) # Clean up
 })
 
 test_that("Calling 'analyse' on a TOF file (sepsis_area) produces a report without errors", {
   infile <- "../../data-raw/sepsis_area.txt"
   outfile <- "../../data-raw/sepsis_area.html"
+
+  suppressWarnings(file.remove(outfile)) # Existing file can hide failed test
+
   analyse(infile)
 
   expect_true(file.exists(outfile))
-  file.remove(outfile)
+  file.remove(outfile) # Clean up
 })
 
 test_that("Calling 'analyse' on a TOF file with >2 classes produces an error", {
   infile <- "../../data-raw/pigs.txt"
   outfile <- "../../data-raw/pigs.html"
+
+  suppressWarnings(file.remove(outfile)) # Existing file can make successful test appear failed
+
   expect_error(analyse(infile))
   expect_false(file.exists(outfile))
 })
+
+context("system tests: GCIMS")
+
+test_that("Calling 'analyse_GCIMS' on a directory of GC-IMS files produces a report without errors", {
+  indir <- "../../data-raw/gcims"
+  outfile <- "../../data-raw/gcims/gcims_analysis.html"
+
+  suppressWarnings(file.remove(outfile)) # Existing file can hide fialed test
+
+  analyse_GCIMS(indir)
+
+  expect_true(file.exists(outfile))
+  file.remove(outfile) # Clean up
+})
+
